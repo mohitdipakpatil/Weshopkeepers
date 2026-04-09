fa7f25efb4968bde8dcea7c9c2018890a6d9847d
Mohit 2811 <mohitdipakpatil@gmail.com>
2026-04-09 22:35:57 +0530

    Fix: Corrected file structure for Railway deployment

--- server/server.js
+++ server/server.js
@@ -54,6 +54,9 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
